6def6f22b85aecb34be9910d8be60db3cd3e11ea
--global <wang.jason.010@gmail.com>
2024-02-25 21:52:03 -0800

    connect-mock-data-to-line-chart

--- src/components/charts/BasicLineChart.tsx
+++ src/components/charts/BasicLineChart.tsx
@@ -1,21 +1,102 @@
+import { fetchMockItemData } from "_redux/slices/itemSlice";
+import { RootState } from "_redux/store";
+import dayjs from "dayjs";
+import { useAppDispatch } from "hooks/useDispatch";
+import { useEffect, useMemo } from "react";
+import { useSelector } from "react-redux";
+
 import { LineChart } from "@mui/x-charts/LineChart";
 
 export default function BasicLineChart() {
+  const dispatch = useAppDispatch();
+
+  const { data, loading, error } = useSelector(
+    (state: RootState) => state.item,
+  );
+
+  // This is a cheap operation, thus prefer to keep it in-line vs on the global store
+  const formattedSaleData = useMemo(
+    () =>
+      data[0]?.sales.reduce((agg: any, sale: any) => {
+        const monthAbbreviation = dayjs(sale.weekEnding)
+          .format("MMM")
+          .toUpperCase();
+
+        if (!agg[monthAbbreviation]) {
+          agg[monthAbbreviation] = {
+            retailSalesSum: 0,
+            wholesaleSalesSum: 0,
+          };
+        }
+
+        agg[monthAbbreviation].retailSalesSum += sale.retailSales;
+        agg[monthAbbreviation].wholesaleSalesSum += sale.wholesaleSales;
+        return agg;
+      }, {}),
+    [data],
+  );
+
+  useEffect(() => {
+    dispatch(fetchMockItemData());
+  }, [dispatch]);
+
+  if (loading || !formattedSaleData) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+
+  const retailSalesSums = Object.values(formattedSaleData).map(
+    (x: any) => x.retailSalesSum,
+  );
+  const wholesaleSalesSums = Object.values(formattedSaleData).map(
+    (x: any) => x.wholesaleSalesSum,
+  );
+  const maxSum = Math.max(...[...retailSalesSums, ...wholesaleSalesSums]);
+
   return (
-    <div style={{ background: "var(--color-pure-white)" }}>
+    <div
+      style={{
+        background: "var(--color-pure-white)",
+        boxShadow: "0",
+      }}
+    >
       <LineChart
-        xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+        title="Retail Sales"
         series={[
           {
-            data: [2, 5.5, 2, 8.5, 1.5, 5],
+            color: "grey",
+            curve: "natural",
+            data: retailSalesSums,
+            showMark: false,
+          },
+          {
+            curve: "natural",
+            data: wholesaleSalesSums,
+            showMark: false,
           },
         ]}
+        xAxis={[
+          {
+            data: Object.keys(formattedSaleData),
+            scaleType: "point",
+            tickLabelStyle: {
+              fontSize: 20,
+            },
+          },
+        ]}
+        yAxis={[{ max: maxSum * 2, min: 0 }]}
+        leftAxis={{
+          labelStyle: { fontSize: 0 },
+        }}
         height={800}
         sx={{
-          "& .MuiDataGrid-cell:hover": {
-            color: "primary.main",
+          // leftAxis Line Styles
+          "& .MuiChartsAxis-left .MuiChartsAxis-line": {
+            strokeWidth: 0,
+          },
+          "& .MuiLineElement-root": {
+            strokeWidth: 5,
           },
           boxShadow: 2,
+          fontFamily: "Product Sans",
         }}
       />
     </div>
--- src/components/item/ItemDetails.tsx
+++ src/components/item/ItemDetails.tsx
@@ -1,3 +1,7 @@
+import itemDetailsStyles from "css/item/ItemDetails.module.css";
+
 export default function ItemDetails() {
-  return <div>Item Details</div>;
+  return (
+    <div className={itemDetailsStyles.itemDetailsContainer}>Item Details</div>
+  );
 }
